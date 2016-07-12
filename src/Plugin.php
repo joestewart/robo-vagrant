38f3106ac6d815d17436c89555c8c0cdc9e98352
Joe Stewart <joe@joestewartjr.com>
2016-07-12 10:24:09 -0500

    Add vagrant plugin subcommands

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,6 +19,66 @@ class Plugin extends Base
 {
     protected $action = 'plugin';
 
+    /**
+     * adds `install` subcommand to vagrant plugin
+     *
+     * @return $this
+     */
+    public function install()
+    {
+        $this->action = 'plugin install';
+
+        return $this;
+    }
+
+    /**
+     * adds `license` subcommand to vagrant plugin
+     *
+     * @return $this
+     */
+    public function license()
+    {
+        $this->action = 'plugin license';
+
+        return $this;
+    }
+
+    /**
+     * adds `list` subcommand to vagrant plugin
+     *
+     * @return $this
+     */
+    public function listPlugins()
+    {
+        $this->action = 'plugin list';
+
+        return $this;
+    }
+
+    /**
+     * adds `uninstall` subcommand to vagrant plugin
+     *
+     * @return $this
+     */
+    public function uninstall()
+    {
+        $this->action = 'plugin uninstall';
+
+        return $this;
+    }
+
+    /**
+     * adds `update` subcommand to vagrant plugin
+     *
+     * @return $this
+     */
+    public function update()
+    {
+        $this->action = 'plugin update';
+
+        return $this;
+    }
+
     public function run()
     {
         $this->printTaskInfo('vagrant plugin' . $this->arguments);
---
+++ tests/VagrantPluginTest.php
@@ -0,0 +1,54 @@
+<?php
+
+class VagrantTestPlugin extends \PHPUnit_Framework_TestCase
+{
+    use \JoeStewart\Robo\Task\Vagrant\loadTasks;
+
+    public function testVagrantPluginInstallCommand()
+    {
+        $command = $this->taskVagrantPlugin('/usr/bin/vagrant')
+            ->install()
+            ->arg('name')
+            ->getCommand();
+        $expected = '/usr/bin/vagrant plugin install name';
+        $this->assertEquals($expected, $command);
+    }
+
+    public function testVagrantPluginLicenseCommand()
+    {
+        $command = $this->taskVagrantPlugin('/usr/bin/vagrant')
+            ->license()
+            ->getCommand();
+        $expected = '/usr/bin/vagrant plugin license';
+        $this->assertEquals($expected, $command);
+    }
+
+    public function testVagrantPluginListCommand()
+    {
+        $command = $this->taskVagrantPlugin('/usr/bin/vagrant')
+            ->listPlugins()
+            ->getCommand();
+        $expected = '/usr/bin/vagrant plugin list';
+        $this->assertEquals($expected, $command);
+    }
+
+    public function testVagrantPluginUninstallCommand()
+    {
+        $command = $this->taskVagrantPlugin('/usr/bin/vagrant')
+            ->uninstall()
+            ->arg('name')
+            ->getCommand();
+        $expected = '/usr/bin/vagrant plugin uninstall name';
+        $this->assertEquals($expected, $command);
+    }
+
+    public function testVagrantPluginUpdateCommand()
+    {
+        $command = $this->taskVagrantPlugin('/usr/bin/vagrant')
+            ->update()
+            ->arg('name')
+            ->getCommand();
+        $expected = '/usr/bin/vagrant plugin update name';
+        $this->assertEquals($expected, $command);
+    }
+}
